Fixed some minor sign problems
Added some plots as well

Sim/Meas_core.py
@@ -35,7 +35,7 @@ def parse_json(js_fname):
             if(beam["phase0"] == None):
                 if(beam["phase0abs"] != None):
                     # beam["phase0"] = beam['phase0abs'] + t*(data['omega0'] + beam['detuning']*data['nu0'])
-                    beam["phase0"] = np.angle(c_exp(t,beam['detuning']*data['nu0'],beam["phase0abs"]))
+                    beam["phase0"] = np.angle(c_exp(t,-beam['detuning']*data['nu0'],beam["phase0abs"]))
                     # beam["phase0"] = beam['phase0abs'] + np.angle(beam["phase0"])
                 else:
                     beam["phase0"] = 0

Sim/Qutip_sims.py
@@ -142,16 +142,12 @@ def QuTiP_LDR(data):
     
 def QuTiP_Cython(data):
 
-    # Set up constants
-    c = const.c
-
     # Set up params
     n_num = data["n_num"]
     state_start = data["n0"]
     omega0 = data['omega0']
     nu0 = data['nu0']
     Omega0 = data['Omega0']
-    z_0 = data['eta0']*c/(omega0 + nu0)
 
     # Set up standard operators
     # Most of these could be called on demand, however 
@@ -191,7 +187,6 @@ def QuTiP_Cython(data):
     options = qtip.Options(atol=1e-8,rtol=1e-8,nsteps=1e6,rhs_reuse=True)
     def run_sim(detuning, state0=state0):
         omega = omega0 + detuning*Omega0
-        eta = z_0*omega/c
         res = qtip.sesolve(H=H_i({'omega' : omega, 'eta' : data['eta0']}),args={'det' : detuning*Omega0},psi0=state0,tlist=ts,options=options)
 
         return res.states
@@ -327,29 +322,16 @@ def QuTiP_C_meas_mult_laser(data):
     data['n0'] = 0
 
     # Set up params
-    nu0 = data['nu0']
     n_num = data['n_num']
-
-    # Set up standard operators
-    # Most of these could be called on demand, however 
-    # caching these will reduce calling overhead
-    sigma_p = qtip.sigmam() # Due to different convention used in previous code
-                            # and internally within QuTiP
 
     a_sum = np.array([[simplified_matrix_data() for _ in range(n_num)] for _ in range(n_num)],dtype=simplified_matrix_data)
     for i in range(n_num-1):
         a_sum[i,i+1] = simplified_matrix_data([entry(val=np.sqrt(i+1),exp=-1)])
         a_sum[i+1,i] = simplified_matrix_data([entry(val=np.sqrt(i+1),exp= 1)])
 
-
-    def H_i(arg,params):
-        return 0*qtip.tensor(qtip.sigmaz()/2,qtip.identity(n_num)) + 0*qtip.tensor(qtip.identity(2),nu0*(qtip.create(n_num)*qtip.destroy(n_num) + qtip.identity(n_num)/2))
-
-    options = qtip.Options(atol=1e-8,rtol=1e-8,nsteps=1e6)
-
     # Prepare states
     p = underlying_solver
-    state0 = qtip.sesolve(H=H_i({'eta' : data['eta0']},p),psi0=state0,tlist=p['ts'],options=options).states
+    state0 = [state0]*p['n_t']
 
     # Simulation run function
     def run_sim(args, state0=state0):
@@ -357,7 +339,7 @@ def QuTiP_C_meas_mult_laser(data):
         res = []
         for j,state in enumerate(state0):
             for i,_ in enumerate(data['beams']):
-                data['beams'][i]['phase0'] = np.angle(c_exp(underlying_solver['ts'][j],nu0*data['beams'][i]['detuning'],data0['beams'][i]['phase0']))
+                data['beams'][i]['phase0'] = np.angle(c_exp(underlying_solver['ts'][j],-data['beams'][i]['detuning'],data0['beams'][i]['phase0']))
             state = qtip.Qobj(state,dims=[[2,n_num],[1,1]])
             res.append(sim_methods[data['params']['solver']](data)(args,state)[-1])
         res = np.array(res)

Sim/Seq_core.py
@@ -2,7 +2,7 @@
 import json
 import numpy as np
 import scipy.constants as const
-from Qutip_sims import *
+from Qutip_sims import sim_methods
 # from Ground_up_sims import Ground_up_full, Ground_up_LDA
 import qutip as qtip
 from c_exp_direct import c_exp
@@ -24,7 +24,7 @@ def parse_json(js_fname):
         t = data['t_prep']
     for d in data['sequence']:
 
-        for i,beam in enumerate(d["beams"]):
+        for _,beam in enumerate(d["beams"]):
 
             if(beam["Omega0"] == None):
                 if(beam["Omega0Hz"] == None):
@@ -35,7 +35,7 @@ def parse_json(js_fname):
             if(beam["phase0"] == None):
                 if(beam["phase0abs"] != None):
                     # beam["phase0"] = beam['phase0abs'] + t*(data['omega0'] + beam['detuning']*data['nu0'])
-                    beam["phase0"] = np.angle(c_exp(t,beam['detuning']*data['nu0'],beam["phase0abs"]))
+                    beam["phase0"] = np.angle(c_exp(t,-beam['detuning']*data['nu0'],beam["phase0abs"]))
                     # beam["phase0"] = beam['phase0abs'] + np.angle(beam["phase0"])
                 else:
                     beam["phase0"] = 0

Sim/plot_results.py
@@ -3,6 +3,34 @@ import numpy as np
 from matplotlib.widgets import Slider
 import matplotlib.pyplot as plt
 
+def multiple_formatter(denominator=2, number=np.pi, latex='\\pi'):
+    def gcd(a, b):
+        while b:
+            a, b = b, a%b
+        return a
+    def _multiple_formatter(x, pos):
+        den = denominator
+        num = np.int(np.rint(den*x/number))
+        com = gcd(num,den)
+        (num,den) = (int(num/com),int(den/com))
+        if den==1:
+            if num==0:
+                return r'$0$'
+            if num==1:
+                return r'$%s$'%latex
+            elif num==-1:
+                return r'$-%s$'%latex
+            else:
+                return r'$%s%s$'%(num,latex)
+        else:
+            if num==1:
+                return r'$\frac{%s}{%s}$'%(latex,den)
+            elif num==-1:
+                return r'$\frac{-%s}{%s}$'%(latex,den)
+            else:
+                return r'$\frac{%s%s}{%s}$'%(num,latex,den)
+    return _multiple_formatter
+
 def load_QuTiP(fname):
     
     d = np.load(fname,allow_pickle=True)
@@ -308,8 +336,8 @@ def plot_seq_scan_projeg(data_pack):
     state_data = np.abs(np.einsum('ijk,ijk->ik',state_data,np.conj(state_data)))
 
     _, ax = plt.subplots()
-    p, = ax.plot(ts,state_data[1,:],label = f"|e>")
-    ax.plot(ts,state_data[0,:],label = f"|g>",linestyle='--', color = p.get_color())
+    _, = ax.plot(ts,state_data[1,:],label = f"|e>")
+    # ax.plot(ts,state_data[0,:],label = f"|g>",linestyle='--', color = p.get_color())
 
     t0 = 0
     for t in t0s:
@@ -322,6 +350,67 @@ def plot_seq_scan_projeg(data_pack):
     # ax2.plot(t,sol[1])q
     # ax.get_xaxis().set_major_formatter(rabi_detuning_format)
     ax.set_xlabel("Time [s]")
+    ax.set_ylabel("p[1]")
+    # ax.set_yscale("logit")
+    ax.grid()
+
+    plt.show()
+
+def plot_meas_scan(data_pack):
+    global ax
+
+    state_data, ts, n_num, t0s = data_pack
+
+    state_data = np.abs(np.einsum('ijk,ijk->ijk',state_data,np.conj(state_data)))
+
+    _, ax = plt.subplots()
+    for i in range(n_num):
+        p, = ax.plot(ts,state_data[1,i,:],label = f"|e,{i}>")
+        ax.plot(ts,state_data[0,i,:],label = f"|g,{i}>",linestyle='--', color = p.get_color())
+
+    t0 = 0
+    for t in t0s:
+        ax.axvline(t0 + t,linestyle='dashdot')
+        t0 += t
+
+    ax.legend()
+    # fig2, ax2 = plt.subplots()
+    # ax2.plot(t,sol[0])
+    # ax2.plot(t,sol[1])q
+    ax.xaxis.set_major_locator(plt.MultipleLocator(np.pi / 2))
+    ax.xaxis.set_minor_locator(plt.MultipleLocator(np.pi / 10))
+    ax.xaxis.set_major_formatter(plt.FuncFormatter(multiple_formatter()))
+    ax.set_xlabel("$\\phi$ [1]")
+    ax.set_ylabel("p[1]")
+    # ax.set_yscale("logit")
+    ax.grid()
+
+    plt.show()
+
+def plot_meas_scan_projeg(data_pack):
+    global ax
+
+    state_data, ts, _, t0s = data_pack
+
+    state_data = np.abs(np.einsum('ijk,ijk->ik',state_data,np.conj(state_data)))
+
+    _, ax = plt.subplots()
+    p, = ax.plot(ts,state_data[1,:],label = f"|e>")
+    ax.plot(ts,state_data[0,:],label = f"|g>",linestyle='--', color = p.get_color())
+
+    t0 = 0
+    for t in t0s:
+        ax.axvline(t0 + t,linestyle='dashdot')
+        t0 += t
+
+    ax.legend()
+    # fig2, ax2 = plt.subplots()
+    # ax2.plot(t,sol[0])
+    # ax2.plot(t,sol[1])q
+    ax.xaxis.set_major_locator(plt.MultipleLocator(np.pi / 2))
+    ax.xaxis.set_minor_locator(plt.MultipleLocator(np.pi / 10))
+    ax.xaxis.set_major_formatter(plt.FuncFormatter(multiple_formatter()))
+    ax.set_xlabel("$\\phi$ [1]")
     ax.set_ylabel("p[1]")
     # ax.set_yscale("logit")
     ax.grid()
@@ -367,8 +456,8 @@ plot_methods = {
     'qutip_seq'             : [load_QuTiP_seq,plot_seq_scan],
     'qutip_seq_projeg'      : [load_QuTiP_seq,plot_seq_scan_projeg],
     'qutip_seq_fockexp'     : [load_QuTiP_seq,plot_seq_scan_Fockexp],
-    'qutip_meas'            : [load_QuTiP_meas,plot_seq_scan],
-    'qutip_meas_projeg'     : [load_QuTiP_meas,plot_seq_scan_projeg],
+    'qutip_meas'            : [load_QuTiP_meas,plot_meas_scan],
+    'qutip_meas_projeg'     : [load_QuTiP_meas,plot_meas_scan_projeg],
     'groundup_time'         : [load_Ground_up,plot_time_scan],
     'groundup_detuning'     : [load_Ground_up,plot_detuning_scan]
 }